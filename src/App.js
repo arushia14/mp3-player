9430e501f3b566cf43758e66a9a1e80b64ba17ff
Arushi A <arushiagarwal14@gmail.com>
2022-11-26 12:18:32 -0500

    song and time working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Song from './components/Song';
 import Player from './components/Player';
 
 import './styles/app.scss';
+import data from './util'
 
 function App() {
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song/>
-      <Player/>
+      <Song currentSong={currentSong}/>
+      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
     </div>
   );
 }
